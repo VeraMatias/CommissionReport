5287cddfa4450e18c6b8beb110e456c0fd5dc9dd
VeraMatias <veramatias93@hotmail.com>
2024-01-29 01:45:00 -0300

    Lectura de dashboard de Firestore

--- src/container/DashboardContainer/DashboardContainer.jsx
+++ src/container/DashboardContainer/DashboardContainer.jsx
@@ -1,14 +1,30 @@
 import './DashboardContainer.css'
 
 import KPICard from '../../components/Dashboard/KPICard/KPICard'
+import { useState, useEffect } from 'react'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useGeneral } from '../../hooks/useGeneral'
 
 const DashboardContainer =  () => {
 
+    const [data, setData] = useState()
+    const { getCollection } = useFirestore()
+    const { formattedNumber } = useGeneral()
+
+    useEffect(() =>{
+        getCollection('overview', setData);
+    },[])
+
     return(
         <div className="container-dashboard">
-            <KPICard color={'purple'} value={'$ 800.000,00'} text={'Balance de Comisiones'}/>
-            <KPICard color={'grey-dark'} value={'$ 1.000.000,00'} text={'Comisiones Pendientes'}/>
-            <KPICard color={'green'} value={'$ 800.000.000,00'} text={'Ventas Totales'}/>
+            {data ? 
+            <>
+                <KPICard color={'purple'} value={formattedNumber(data[0].balance)} text={'Balance de Comisiones'}/>
+                <KPICard color={'grey-dark'} value={formattedNumber(data[0].pending)} text={'Comisiones Pendientes'}/>
+                <KPICard color={'green'} value={formattedNumber(data[0].sales)} text={'Ventas Totales'}/>
+            </>
+            :
+            null}
         </div>
     )
 }
