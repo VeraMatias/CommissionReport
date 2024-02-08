377bea46ead5db1933b0d0eb748c8f3dea516086
VeraMatias <veramatias93@hotmail.com>
2024-02-08 00:52:22 -0300

    Cambio de class a className en OrderForm

--- src/components/Orders/OrderForm/OrderForm.jsx
+++ src/components/Orders/OrderForm/OrderForm.jsx
@@ -6,32 +6,32 @@ const OrderForm = () =>{
     const { handleInputChange, handleCreate } = useForm()
     
 return (
-    <form class="order-form" id='order-form' onSubmit={handleCreate('orders')}>
+    <form className="order-form" id='order-form' onSubmit={handleCreate('orders')}>
         <p className='heading'>Nueva Venta</p>
-        <div class="field">
-            <input placeholder="Numero de CPQ" class="input-field" type="text" name='cpq' onChange={handleInputChange}></input>
+        <div className="field">
+            <input placeholder="Numero de CPQ" className="input-field" type="text" name='cpq' onChange={handleInputChange}></input>
         </div>
-        <div class="field">
-            <input placeholder="Factura" class="input-field" type="text" name='invoice' onChange={handleInputChange}></input>
+        <div className="field">
+            <input placeholder="Factura" className="input-field" type="text" name='invoice' onChange={handleInputChange}></input>
         </div>
-        <div class="field">
-            <input placeholder="Monto" class="input-field" type="number" name='amount' onChange={handleInputChange}></input>
+        <div className="field">
+            <input placeholder="Monto" className="input-field" type="number" name='amount' onChange={handleInputChange}></input>
         </div>
-        <div class="field">
-            <input placeholder="IVA" class="input-field" type="text" name='IVA' onChange={handleInputChange}></input>
+        <div className="field">
+            <input placeholder="IVA" className="input-field" type="text" name='IVA' onChange={handleInputChange}></input>
         </div>
-        <div class="field">
-            <input placeholder="% de Comisión" class="input-field" type="text" name='commission' onChange={handleInputChange}></input>
+        <div className="field">
+            <input placeholder="% de Comisión" className="input-field" type="text" name='commission' onChange={handleInputChange}></input>
         </div>
-        <div class="field checkbox">
+        <div className="field checkbox">
             <p className='checkbox-text'>Pagada?</p>
-            <input  class="input-field" type="checkbox" name='paid' onChange={handleInputChange}></input>
+            <input  className="input-field" type="checkbox" name='paid' onChange={handleInputChange}></input>
         </div>
-        <div class="field checkbox">
+        <div className="field checkbox">
             <p className='checkbox-text'>Comisionada?</p>
-            <input  class="input-field" type="checkbox" name='commissioned' onChange={handleInputChange}></input>
+            <input  className="input-field" type="checkbox" name='commissioned' onChange={handleInputChange}></input>
         </div>
-        <button class="button3">Crear</button>
+        <button className="button3">Crear</button>
     </form>
 )
 }
