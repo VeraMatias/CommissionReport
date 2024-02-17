baea0d65bbb1f600ff1c74ff180c7ecac1422e17
VeraMatias <veramatias93@hotmail.com>
2024-02-17 18:59:41 -0300

    Centrado de Edit form Order

--- src/components/Orders/EditOrder/EditOrder.jsx
+++ src/components/Orders/EditOrder/EditOrder.jsx
@@ -16,30 +16,33 @@ const EditOrder = () =>{
     
 return (
     dataForm ?
-    <form className="order-form" id='order-form' onSubmit={handleUpdate('orders', itemID)}>
-        <p className='heading'>Editar Venta</p>
-        <div className="field">
-            <span className='text-edit'>CPQ</span>
-            <input placeholder="Numero de CPQ" className="input-field" type="text" name='cpq' onChange={handleInputChange} value={dataForm.cpq}></input>
-        </div>
-        <div className="field">
-            <span className='text-edit'>Factura</span>
-            <input placeholder="Factura" className="input-field" type="text" name='invoice' onChange={handleInputChange} value={dataForm.invoice}></input>
-        </div>
-        <div className="field">
-            <span className='text-edit'>Monto</span>
-            <input placeholder="Monto" className="input-field" type="number" name='amount' onChange={handleInputChange} value={dataForm.amount}></input>
-        </div>
-        <div className="field">
-            <span className='text-edit'>IVA</span>
-            <input placeholder="IVA" className="input-field" type="text" name='IVA' onChange={handleInputChange} value={dataForm.IVA}></input>
-        </div>
-        <div className="field">
-            <span className='text-edit'>Comisión %</span>
-            <input placeholder="% de Comisión" className="input-field" type="text" name='commission' onChange={handleInputChange} value={dataForm.commission}></input>
-        </div>
-        <button className="button3">Editar</button>
-    </form>
+    <div className="order-edit-container">
+        <form className="order-form" id='order-form' onSubmit={handleUpdate('orders', itemID)}>
+            <p className='heading'>Editar Venta</p>
+            <div className="field">
+                <span className='text-edit'>CPQ</span>
+                <input placeholder="Numero de CPQ" className="input-field" type="text" name='cpq' onChange={handleInputChange} value={dataForm.cpq}></input>
+            </div>
+            <div className="field">
+                <span className='text-edit'>Factura</span>
+                <input placeholder="Factura" className="input-field" type="text" name='invoice' onChange={handleInputChange} value={dataForm.invoice}></input>
+            </div>
+            <div className="field">
+                <span className='text-edit'>Monto</span>
+                <input placeholder="Monto" className="input-field" type="number" name='amount' onChange={handleInputChange} value={dataForm.amount}></input>
+            </div>
+            <div className="field">
+                <span className='text-edit'>IVA</span>
+                <input placeholder="IVA" className="input-field" type="text" name='IVA' onChange={handleInputChange} value={dataForm.IVA}></input>
+            </div>
+            <div className="field">
+                <span className='text-edit'>Comisión %</span>
+                <input placeholder="% de Comisión" className="input-field" type="text" name='commission' onChange={handleInputChange} value={dataForm.commission}></input>
+            </div>
+            <button className="button3">Editar</button>
+        </form>
+    </div>
+
     : null
 )
 }
