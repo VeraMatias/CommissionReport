5627fb050f68e031293fab2764c298bc31e69be7
VeraMatias <veramatias93@hotmail.com>
2024-02-20 22:56:48 -0300

    Mobile OrderForm PaycheckForm

--- src/components/Orders/OrderForm/OrderForm.jsx
+++ src/components/Orders/OrderForm/OrderForm.jsx
@@ -8,28 +8,30 @@ const OrderForm = () =>{
 return (
     <form className="order-form" id='order-form' onSubmit={handleCreate('orders')}>
         <p className='heading'>Nueva Venta</p>
-        <div className="field">
-            <input placeholder="Numero de CPQ" className="input-field" type="text" name='cpq' onChange={handleInputChange}></input>
-        </div>
-        <div className="field">
-            <input placeholder="Factura" className="input-field" type="text" name='invoice' onChange={handleInputChange}></input>
-        </div>
-        <div className="field">
-            <input placeholder="Monto" className="input-field" type="number" name='amount' onChange={handleInputChange}></input>
-        </div>
-        <div className="field">
-            <input placeholder="IVA" className="input-field" type="text" name='IVA' onChange={handleInputChange}></input>
-        </div>
-        <div className="field">
-            <input placeholder="% de Comisión" className="input-field" type="text" name='commission' onChange={handleInputChange}></input>
-        </div>
-        <div className="field checkbox">
-            <p className='checkbox-text'>Pagada?</p>
-            <input  className="input-field" type="checkbox" name='paid' onChange={handleInputChange}></input>
-        </div>
-        <div className="field checkbox">
-            <p className='checkbox-text'>Comisionada?</p>
-            <input  className="input-field" type="checkbox" name='commissioned' onChange={handleInputChange}></input>
+        <div className="form-container-field">
+            <div className="field">
+                <input placeholder="Numero de CPQ" className="input-field" type="text" name='cpq' onChange={handleInputChange}></input>
+            </div>
+            <div className="field">
+                <input placeholder="Factura" className="input-field" type="text" name='invoice' onChange={handleInputChange}></input>
+            </div>
+            <div className="field">
+                <input placeholder="Monto" className="input-field" type="number" name='amount' onChange={handleInputChange}></input>
+            </div>
+            <div className="field">
+                <input placeholder="IVA" className="input-field" type="text" name='IVA' onChange={handleInputChange}></input>
+            </div>
+            <div className="field">
+                <input placeholder="% de Comisión" className="input-field" type="text" name='commission' onChange={handleInputChange}></input>
+            </div>
+            <div className="field checkbox">
+                <p className='checkbox-text'>Pagada?</p>
+                <input  className="input-field" type="checkbox" name='paid' onChange={handleInputChange}></input>
+            </div>
+            <div className="field checkbox">
+                <p className='checkbox-text'>Comisionada?</p>
+                <input  className="input-field" type="checkbox" name='commissioned' onChange={handleInputChange}></input>
+            </div>
         </div>
         <button className="button3">Crear</button>
     </form>
